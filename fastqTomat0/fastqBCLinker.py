599bc49dc642729d8eafea4f3803c00eec002d69
CJackson <christopher@cajackson.org>
2018-09-17 12:25:00 -0400

    Pandas

--- fastqTomat0/fastqBCLinker.py
+++ fastqTomat0/fastqBCLinker.py
@@ -7,6 +7,9 @@ from fastqTomat0.lib.barcode_indexer import Link10xBCCounts, nest_dict
 import argparse
 import multiprocessing
 
+import pandas as pd
+import numpy as np
+
 # These are the minimum quality scores required to consider a sequence as a valid barcode
 BC_MIN_QUAL = 25
 UMI_MIN_QUAL = 15
@@ -47,7 +50,7 @@ def main():
                   bc2_seed=args.bc_seed, bc2_len=args.bc_len)
 
 
-def link_barcodes(bc_fastq_1, bc_fastq_2, bc_fastq_3, out_file_path, is_zipped=False, allowed_indexes=None,
+def link_barcodes(bc_fastq_1, bc_fastq_2, bc_fastq_3, out_file_path=None, is_zipped=False, allowed_indexes=None,
                   max_index_mismatch=1, bc1_pattern=PATTERN, bc2_seed=BC2_SEED, bc2_len=BC2_LENGTH, bc2_mapfile=None):
     assert len(bc_fastq_1) == len(bc_fastq_2)
 
@@ -64,40 +67,46 @@ def link_barcodes(bc_fastq_1, bc_fastq_2, bc_fastq_3, out_file_path, is_zipped=F
     for bc_dict in mp_pool.imap_unordered(linker.parse_fastq_mp, zip(bc_fastq_1, bc_fastq_2, bc_fastq_3)):
         bcs = merge_bcs(bcs, bc_dict)
 
-    output_bcs(out_file_path, bcs, allowed_indexes=allowed_indexes, max_index_mismatch=max_index_mismatch,
-               bc2_map=bc2_map)
+    bc_df = output_bcs(bcs, allowed_indexes=allowed_indexes, max_index_mismatch=max_index_mismatch, bc2_map=bc2_map)
+    if out_file_path is not None:
+        bc_df.to_csv(out_file_path, sep="\t")
+    return bc_df
 
+def output_bcs(bc_dict, allowed_indexes=None, bc2_map=None, max_index_mismatch=1, max_bc_mismatch=1):
 
-def output_bcs(out_file_path, bc_dict, allowed_indexes=None, bc2_map=None, max_index_mismatch=1, max_bc_mismatch=1):
+    # Create an empty DataFrame
+    columns = ["Index","Cell_Barcode","Genotype","UMI_Count"]
+    bc_df = pd.DataFrame(np.zeros((0,4), dtype=np.dtype(object)), columns=columns)
+    bc_df['UMI_Count'] = bc_df['UMI_Count'].astype(np.dtype(int))
 
-    with open(out_file_path, mode="w") as outfh:
-        for idx, idx_dict in bc_dict.items():
-            if allowed_indexes is not None:
-                # See if the index is in the keep list, or if it's within max_mismatch of the keep list
-                try:
-                    idx = reindex_for_mismatches(idx, allowed_indexes, max_mismatch=max_index_mismatch)
-                except IndexError:
-                    continue
-            else:
-                pass
+    for idx, idx_dict in bc_dict.items():
+        if allowed_indexes is not None:
+            # See if the index is in the keep list, or if it's within max_mismatch of the keep list
+            try:
+                idx = reindex_for_mismatches(idx, allowed_indexes, max_mismatch=max_index_mismatch)
+            except IndexError:
+                continue
+        else:
+            pass
 
-            # If the index is OK, print all of the BC1, BC2, UMI counts for that index
-            for bc1, bc2_dict in idx_dict.items():
+        # If the index is OK, print all of the BC1, BC2, UMI counts for that index
+        for bc1, bc2_dict in idx_dict.items():
 
-                # Merge together the UMI counts from barcodes that have an acceptable number of mismatches
-                if bc2_map is not None and max_bc_mismatch > 0:
-                    bc2_dict = merge_mismatches(bc2_dict, list(bc2_map.keys()), max_mismatch=max_bc_mismatch)
+            # Merge together the UMI counts from barcodes that have an acceptable number of mismatches
+            if bc2_map is not None and max_bc_mismatch > 0:
+                bc2_dict = merge_mismatches(bc2_dict, list(bc2_map.keys()), max_mismatch=max_bc_mismatch)
 
-                # Print the Index, Barcode 1, Barcode 2, and UMI counts as a TSV
-                for bc2, umi_set in bc2_dict.items():
-                    if bc2_map is not None:
-                        try:
-                            bc2 = bc2_map[bc2]
-                        except KeyError:
-                            pass
+            # Print the Index, Barcode 1, Barcode 2, and UMI counts
+            for bc2, umi_set in bc2_dict.items():
+                if bc2_map is not None:
+                    try:
+                        bc2 = bc2_map[bc2]
+                    except KeyError:
+                        pass
 
-                    print("\t".join([idx, bc1, bc2, str(len(umi_set))]), file=outfh)
+                bc_df.append(dict(Index=idx, Cell_Barcode=bc2, Genotype=bc2, UMI_Count=len(umi_set)), ignore_index=True)
 
+    return bc_df
 
 def merge_bcs(bc_dict1, bc_dict2):
     for idx, level_1_dict in bc_dict2.items():
