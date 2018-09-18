5935d6a000c4b0af0612ef3056a3df1e50e775bf
CJackson <christopher@cajackson.org>
2018-09-18 13:22:44 -0400

    Finish main

--- fastqTomat0/tenXtomatrix.py
+++ fastqTomat0/tenXtomatrix.py
@@ -4,12 +4,15 @@ from fastqTomat0.lib.barcode_indexer import IDX, BARCODE, GENOTYPE
 import pandas as pd
 import argparse
 
+
 def main():
     ap = argparse.ArgumentParser(description="Process 10x Files into a Matrix File")
     ap.add_argument("-p", "--path", dest="path", help="Input File PATH", metavar="PATH", required=True)
     ap.add_argument("-b", "--bc", dest="bc", help="Input Barcode-Genotype Map FILE", metavar="FILE", default=None)
+    ap.add_argument("-i", "--bc_idx", dest="bc_idx", help="Barcode Library INDEX", metavar="INDEX", default=None)
     ap.add_argument("-o", "--output", dest="out", help="Output TSV FILE", metavar="FILE", required=True)
-
+    args = ap.parse_args()
+    tenX_to_matrix(args.path, bc_file=args.bc, bc_file_lib_index=args.bc_idx, outfile_path=args.out)
 
 
 def tenX_to_matrix(tenX_path, bc_file=None, bc_file_lib_index=None, outfile_path=None):
@@ -31,3 +34,7 @@ def filter_barcodes(tenX_df, barcode_df):
     tenX_df = tenX_df.loc[barcode_df.index.intersection(tenX_df.index)]
     tenX_df = tenX_df.merge(barcode_df[GENOTYPE])
     return tenX_df
+
+
+if __name__ == '__main__':
+    main()
