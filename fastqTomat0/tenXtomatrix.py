a584387af7f65eea0417ed3ae0846c91c6523425
CJackson <christopher@cajackson.org>
2018-09-18 17:04:55 -0400

    Bulking

--- fastqTomat0/tenXtomatrix.py
+++ fastqTomat0/tenXtomatrix.py
@@ -11,11 +11,14 @@ def main():
     ap.add_argument("-b", "--bc", dest="bc", help="Input Barcode-Genotype Map FILE", metavar="FILE", default=None)
     ap.add_argument("-i", "--bc_idx", dest="bc_idx", help="Barcode Library INDEX", metavar="INDEX", default=None)
     ap.add_argument("-o", "--output", dest="out", help="Output TSV FILE", metavar="FILE", required=True)
+    ap.add_argument("--bulkup", dest="bulk", help="Unzip FASTQ", action='store_const', const=True, default=False)
     args = ap.parse_args()
-    tenX_to_matrix(args.path, bc_file=args.bc, bc_file_lib_index=args.bc_idx, outfile_path=args.out)
+    tenX_to_matrix(args.path, bc_file=args.bc, bc_file_lib_index=args.bc_idx, outfile_path=args.out,
+                   bulk_up_genotypes=args.bulk)
 
 
-def tenX_to_matrix(tenX_path, bc_file=None, bc_file_lib_index=None, outfile_path=None, remove_doublets=True):
+def tenX_to_matrix(tenX_path, bc_file=None, bc_file_lib_index=None, outfile_path=None, remove_doublets=True,
+                   bulk_up_genotypes=False):
 
     if bc_file is not None:
         bc = pd.read_table(bc_file, sep="\t")
@@ -25,9 +28,12 @@ def tenX_to_matrix(tenX_path, bc_file=None, bc_file_lib_index=None, outfile_path
             bc.drop_duplicates(subset=BARCODE, keep=False)
         df = tenX.tenXProcessor(file_path=tenX_path, allowed_barcodes=bc.index.tolist()).process_files()
         df = filter_barcodes(df, bc)
+
+        if bulk_up_genotypes:
+            df.groupby(GENOTYPE).sum()
+
     else:
         df = tenX.tenXProcessor(file_path=tenX_path).process_files()
-
 
     if outfile_path is not None:
         df.to_csv(outfile_path, sep="\t")
