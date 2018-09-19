ad217f0cb9b8b49e57fb96d77acf709e71238e2d
CJackson <christopher@cajackson.org>
2018-09-18 20:42:36 -0400

    Ugh

--- fastqTomat0/lib/fasta.py
+++ fastqTomat0/lib/fasta.py
@@ -15,19 +15,25 @@ def fasta_to_dict(fh, key_type='id', require_unique=True):
             continue
         elif line[0] == ">":
             if current_key is not None:
-
-                if key_type is 'id':
-                    if require_unique and current_key in fasta_dict:
-                        raise KeyError("Sequence ID is not unique")
-                    fasta_dict[current_key] = current_seq
-                if key_type is 'seq':
-                    if require_unique and current_seq in fasta_dict:
-                        raise KeyError("Sequence ID is not unique")
-                    fasta_dict[current_seq] = current_key
+                add_to_dict(fasta_dict, current_key, current_seq, require_unique, key_type)
 
             current_key = line[1:]
             current_seq = ""
         else:
             current_seq += line
 
+    if current_key is not None and current_seq != "":
+        add_to_dict(fasta_dict, current_key, current_seq, require_unique, key_type)
+
     return fasta_dict
+
+
+def add_to_dict(fasta_dict, key, seq, require_unique=True, key_type='id'):
+    if key_type is 'id':
+        if require_unique and key in fasta_dict:
+            raise KeyError("Sequence ID is not unique")
+        fasta_dict[key] = seq
+    if key_type is 'seq':
+        if require_unique and seq in fasta_dict:
+            raise KeyError("Sequence ID is not unique")
+        fasta_dict[seq] = key
